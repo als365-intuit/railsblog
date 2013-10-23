c098ffc7bb7507aabee2d44d720a5b2c53bfbf75
Austin Stanhope <Austin_Stanhope@Intuit.com>
2013-10-23 14:57:30 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsblog::Application.config.secret_token = '34c32791926e706a425387b43e07ae5677f1304235b9d72506e43ce10ee115ddbc9c968b36e7eab976b0bdbf790281b0a171918b764a398d41af35539577d6e6'
